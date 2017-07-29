a8eda378160c57c13c7cf2e0bad7c7c5298841a6
xPMo <PMo_@vivaldi.net>
2017-07-29 01:17:04 -0500

    termux-url-opener initial draft

---
+++ BIN/termux-url-opener.sh
@@ -0,0 +1,28 @@
+#!/data/data/com.termux/files/usr/bin/zsh
+echo "extract audio? [y/n/o]"
+read y
+case $y in
+	y)
+		cd Music/youtube-dl
+		youtube-dl -x $1 
+	;;
+	n)
+		cd Videos
+		youtube-dl $1
+	;;
+	*)
+		echo "Okay, you're in $(pwd), something to do first? [y/n]"
+		read y
+		while [$y != 'n' ]; do
+			read cmd
+			$cmd
+			echo "Okay, you're in $(pwd), something else to do first? [y/n]"
+			read y
+		done
+		echo "Arguments for youtube-dl?"
+		read cmd
+		youtube-dl $cmd $1
+	;;
+esac
+
+
