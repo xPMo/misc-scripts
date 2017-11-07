0fa18433b81ea69600c80d3dcff445bcb96bdc44
xPMo <PMo_@vivaldi.net>
2017-11-07 17:13:14 -0600

    Copy instead of linking when on Android (workaround FAT32 exec bit problem)

--- init.sh
+++ init.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
+shopt -s dotglob
+if [[ -n $ANDROID_DATA ]]; then
+	link_by() {
+		echo Copying "$1 -> $2"
+		cp $1 $2; chmod +x $2
+	}
+else
+	link_by() {
+		echo Creating link "$2 -> $1"
+		ln -s $1 $2
+	}
+fi
 cd BIN
 for file in * 
 do
-	echo Symlinking $file
-	ln -s $(pwd)/$file $HOME/.local/bin/${file%.*}
+	echo $file
+	link_by $(pwd)/$file $HOME/.local/bin/${file%.*}
 done
