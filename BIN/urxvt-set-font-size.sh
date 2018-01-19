fcdb5c09f03c24cd9804a5f12f800525273150fb
xPMo <PMo_@vivaldi.net>
2018-01-19 14:54:57 -0600

    add urxvt font size script

---
+++ BIN/urxvt-set-font-size.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if echo $1 | egrep -q '^[0-9]+(.5)?$'; then
+	newfont=$(xrdb -query | grep 'urxvt\*font:' | cut -f 2- | sed "s/size=[0-9.]*/:size=$1/")
+else
+	newfont=$(xrdb -query | grep 'urxvt\*font:' | cut -f 2-)
+fi
+echo -e "\033]710;$newfont\033\\"
