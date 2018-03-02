616a097b6b81a47986464e96341a93ae1f9808d8
xPMo <PMo_@vivaldi.net>
2018-03-02 16:39:42 -0600

    generalize urxvt-set-* for more escape codes

--- BIN/urxvt-set-font-size.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if echo $1 | egrep -q '^[0-9]+(.5)?$'; then
-	newfont=$(xrdb -query | grep 'urxvt\*font:' | cut -f 2- | sed "s/size=[0-9.]*/:size=$1/")
-else
-	newfont=$(xrdb -query | grep 'urxvt\*font:' | cut -f 2-)
-fi
-echo -ne "\033]710;$newfont\033\\"
