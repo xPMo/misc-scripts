fa550c5e824874a36e7f48e72cf186b28e2f50b0
xPMo <PMo_@vivaldi.net>
2018-03-07 16:28:51 -0600

    I forget why, but this is better for some reason

--- BIN/snapper-diff.sh
+++ BIN/snapper-diff.sh
@@ -7,14 +7,15 @@ through \`less\`.
 
 COMPARE: (N, M whole numbers:)
 	N         	Diff snapshot N with snapshot 0 (current state)
-	-N        	Diff the Nth most recent snapshot with snapshot 0
-	dN        	Diff the Nth most recent pre-post pair
+	bN        	Diff the Nth most recent snapshot with snapshot 0
+	pN        	Diff the Nth most recent pre-post pair
 	N..M      	Diff the snapshots N and M
 
 	-f        	call fzf on the diff list. Use MATCH as the initial query
 	-c CONFIG 	use CONFIG with snapper instead of the default
 "
 snapper_cmd="snapper"
+
 differ() {
 	local files=()
 	trap '{ rm "${files[@]}"; exit }' INT
@@ -39,8 +40,9 @@ while getopts ":fc:" opt; do
 	case "${opt}" in
 	f)
 		differ(){
+			echo $s
 			fzf --query="${1:-""}" --multi --ansi --preview \
-				"snapper diff $s {+} \
+				"snapper diff \"$s\" {+} \
 				| sed -e \"2 s/^/$bgrn/; 1 s/^/$bred/; s/^+/$grn+/; s/^-/$red-/; s/^@/$blu/; s/^ /$res/\"" \
 				--preview-window=up:60%
 		}
@@ -53,16 +55,15 @@ shift $(( OPTIND - 1))
 if [ "$#" -eq 0 ]; then
 	echo "$USAGE"
 	exit 1
-elif [[ $1 =~ ^[a-z][0-9]*$ ]]; then
+elif [[ $1 =~ ^p[0-9]*$ ]]; then
 	# compare n-th most recent pre-post pair
-	line=${1:1}
+	line="${1:1}"
 	s="$($snapper_cmd list --type pre-post | tail -n"${line:-1}" | head -n1 | cut -d\| -f -2 | xargs | sed 's/ | /../')"
 elif [[ $1 =~ ^[0-9]+..[0-9]+$ ]]; then
 	# raw pair
 	s="$1"
-elif (( $1 < 0 )); then
-	# -n snapshots ago
-	s="$($snapper_cmd list | tail -n$(( -$1 )) | head -n1 | cut -d\| -f2 | xargs)..0"
+elif [[ $1 =~ ^b[0-9]*$ ]]; then
+	s="$($snapper_cmd list | tail -n$(( ${1:1} )) | head -n1 | cut -d\| -f2 | xargs)..0"
 elif (( $1 > 0 )); then
 	# snapshot value
 	s="$(( $1 ))..0"
