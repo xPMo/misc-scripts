f36e8d664abd85e0e8171528c901ef17b1f7084f
xPMo <PMo_@vivaldi.net>
2018-04-25 03:13:33 -0500

    Use git's -C flag

--- BIN/gitU.sh
+++ BIN/gitU.sh
@@ -3,11 +3,11 @@ IFS=$'\n'
 # assumes $GIT is a directory containing only git repos
 if [[ "$1" =~ c ]]; then
 	function git_command {
-		git gc 2> /dev/null && echo "$b${dir##*/}$n done"
+		git -C $dir gc 2> /dev/null && echo "$b${dir##*/}$n done"
 	}
 else
 	function git_command {
-		git -c color.ui=always pull --recurse-submodules=on-demand 2> /dev/null \
+		git -C $dir -c color.ui=always pull --recurse-submodules=on-demand 2> /dev/null \
 			| sed -e "s/\(Already\|remote\)/$cr$b${dir##*/}$n/" \
 			      -e "s/^Updating/$b${dir##*/}$n:/"
 	}
@@ -22,7 +22,6 @@ command -v pass > /dev/null 2>&1 \
 	&& PASSWORD_STORE_DIR="$HOME/.password-store"
 
 for dir in $GIT/* $PASSWORD_STORE_DIR; do
-	cd "$dir"
 	git_command &
 done
 wait 
