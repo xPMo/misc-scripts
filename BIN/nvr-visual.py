ec824856d60bf9c8654012fe6a90fd7894c8bc69
xPMo <PMo_@vivaldi.net>
2018-05-14 18:51:01 -0500

    oneline it

--- BIN/nvr-visual.py
+++ BIN/nvr-visual.py
@@ -7,7 +7,8 @@ import sys
 from nvr.nvr import main
 
 if __name__ == '__main__':
-    a = ([re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])]
+    sys.exit(main(
+        argv=([re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])]
          + ['-s', '-c', 'doautocmd BufEnter']
          + sys.argv[1:])
-    sys.exit(main(argv=a))
+     ))
