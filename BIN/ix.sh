1d781b15ec5cea0a8966ef4bebdd548b383742bc
xPMo <PMo_@vivaldi.net>
2018-08-12 09:34:49 -0500

    make ix better

--- BIN/ix.sh
+++ BIN/ix.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Examples:
 #     ix hello.txt              # paste file (name/ext will be set).
@@ -6,30 +6,39 @@
 #     ix -n 1 self_destruct.txt # paste will be deleted after one read.
 #     ix -i ID hello.txt        # replace ID, if you have permission.
 #     ix -d ID
+IFS='
+	'
 
-ix() {
-    local opts
-    local OPTIND
-    [ -f "$HOME/.netrc" ] && opts='-n'
-    while getopts ":hd:i:n:" x; do
-        case $x in
-            h) echo "ix [-d ID] [-i ID] [-n N] [opts]"; return;;
-            d) $echo curl $opts -X DELETE ix.io/$OPTARG; return;;
-            i) opts="$opts -X PUT"; local id="$OPTARG";;
-            n) opts="$opts -F read:1=$OPTARG";;
-        esac
-    done
-    shift $((OPTIND - 1))
-    [ -t 0 ] && {
-        local filename="$1"
-        shift
-        [ "$filename" ] && {
-            curl $opts -F f:1=@"$filename" $* ix.io/$id
-            return
-        }
-        echo "^C to cancel, ^D to send."
-    }
-    curl $opts -F f:1='<-' $* ix.io/$id
-}
+[ -f "$HOME/.netrc" ] && opts='-n'
+while getopts ":hd:i:n:" x; do
+	case $x in
+		h) 
+			cat >&2 << EOF
+ix [-d ID] [-i ID] [-n N] [curl opts]
 
-ix $*
+	-d ID   delete the given ID
+	-i ID   replace the given ID
+	-n N    delete the paste after N reads
+
+Curl can work with a filename or stdin. If a filename is
+provided, ix.io will set the name on the paste.
+EOF
+			exit 0;;
+		d)
+			curl $opts -X DELETE ix.io/$OPTARG
+			exit $?;;
+		i) opts="$opts -X PUT"; local id="$OPTARG";;
+		n) opts="$opts -F read:1=$OPTARG";;
+	esac
+done
+shift $((OPTIND - 1))
+if [ -t 0 ]; then
+	local filename="$1"
+	shift
+	[ "$filename" ] && {
+		curl $opts -F f:1=@"$filename" $@ ix.io/$id
+		exit $?
+	}
+	echo "^C to cancel, ^D to send."
+fi
+curl $opts -F f:1='<-' "$@" ix.io/$id
