a1e79829580c3213b967a5ca8154bf08c32d0a88
xPMo <PMo_@vivaldi.net>
2018-12-22 02:08:44 -0600

    termux-url-opener better(?)

--- termux-BIN/termux-url-opener.sh
+++ termux-BIN/termux-url-opener.sh
@@ -1,9 +1,11 @@
-#!/data/data/com.termux/files/usr/bin/zsh
+#!/data/data/com.termux/files/usr/bin/bash
 br="\033[1;31m%s\033[0m"
 lock="${PREFIX:-/data/data/com.termux/files/usr}/tmp/.url.queue.lock"
 queue="$HOME/url.queue"
 
-function get_f {
+# specify a variable name, and use fzf to set a useful value to it
+# (like a filename for output)
+get_f() {
 	echo -n "Specify variable name: "
 	read y
 	if (( ${+y} )); then
@@ -14,8 +16,23 @@ function get_f {
 		echo -n "No variable name given."
 	fi
 }
-function get_d {
+
+# cd to a directory using fzf
+get_d() {
 	cd $(find -L / -mindepth 1 \( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \) -prune -o -type d -print | fzf)
+}
+
+# print help for cutomizing script
+print_opts() {
+	(( ${+_h} )) && printf "%s$br%s\n" "get $_c " h "elp"
+	printf "%s$br%s\n" "print this " H "elp"
+	printf "%s$br%s\n" "specify " p "ath"
+	printf "%s$br%s\n" "make new " F "ile"
+	printf "%s$br%s\n" "specify " d "irectory to run command in"
+	printf "%s$br%s\n" "make new " D "irectory"
+	printf "%s$br%s\n" "provide " a "rguments"
+	echo -e "\nAvoid setting _c, _h, br, y, queue, lock, _tmp"
+	print -rD $PWD
 }
 
 if (( ${+1} )); then
@@ -23,20 +40,24 @@ if (( ${+1} )); then
 	printf "$br%s\n" W "get, add arguments"
 	printf "%s$br%s\n" "youtube-dl, e" x "tract audio"
 	printf "$br%s\n" y "outube-dl"
+	printf "%s$br%s\n" "youtube-dl, choose " f "ormat"
 	printf "$br%s\n" Y "outube-dl, add arguments"
 	read y
 	case $y in
 
 	# Default commands
 	w)
-		echo "cd $HOME/Downloads; wget --continue \"$1\"" >> $queue
-	;;
+		echo "cd $HOME/Downloads; wget --continue \"$1\"" >> $queue ;;
 	x)
-		echo "cd $HOME/Music/youtube-dl; youtube-dl -x \"$1\"" >> $queue
-	;;
+		echo "cd $HOME/Music/youtube-dl; youtube-dl -x \"$1\"" >> $queue ;;
 	y)
-		echo "cd $HOME/Videos; youtube-dl \"$1\"" >> $queue
-	;;
+		echo "cd $HOME/Videos/youtube-dl; youtube-dl \"$1\"" >> $queue ;;
+	f|F)
+		format="$(youtube-dl -F "$1" | sed -n '/^[0-9]/p' |
+			fzf --multi --prompt='Format(s): ' | cut -d\  -f1)"
+		format="${format%%'+'}"
+		echo "cd $HOME/Videos/youtube-dl; youtube-dl \"$1\" -f $format" >> $queue
+		;;
 
 	# Fined-grained control
 	W)
@@ -48,17 +69,6 @@ if (( ${+1} )); then
 		_h="youtube-dl --help"
 		;&
 	*)
-		function print_opts {
-			(( ${+_h} )) && printf "%s$br%s\n" "get $_c " h "elp"
-			printf "%s$br%s\n" "print this " H "elp"
-			printf "%s$br%s\n" "specify " p "ath"
-			printf "%s$br%s\n" "make new " F "ile"
-			printf "%s$br%s\n" "specify " d "irectory to run command in"
-			printf "%s$br%s\n" "make new " D "irectory"
-			printf "%s$br%s\n" "provide " a "rguments"
-			echo -e "\nAvoid setting _c, _h, br, y, queue, lock, _tmp"
-			print -rD $PWD
-		}
 		if [[ ! -n $_c ]]; then
 			echo "Specify command to run:"
 			read _c
@@ -121,7 +131,7 @@ trap 'sig=1' SIGINT
 touch $lock
 remain=$(mktemp "$PREFIX/tmp/.url.queue.XXXXXX")
 while read line; do
-	if [ $sig ]; then
+	if [[ "${sig:-}" ]]; then
 		# Don't run commands once signal recieved
 		echo $line >> $remain
 		(( total += 1 ))
@@ -134,7 +144,7 @@ mv $remain $queue
 rm $lock
 # Notify user of $queue status
 #
-if  ((success == total )); then 
+if  (( success == total )); then
 	termux-notification --id url --title "Termux URL Opener | Termux:API" \
 		--content "All URLs processed successfully. ($total/$total)"
 else
