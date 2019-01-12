3dba93c7a789a192ed0fb31e3c0aaf9655316bf3
xPMo <PMo_@vivaldi.net>
2019-01-12 15:27:58 -0600

    screenkey wrapper using xresources

---
+++ BIN/screenkey-wrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# shellcheck disable=2046
+exec screenkey --font-size small $(xrdb -query | awk -f /dev/fd/3 3<< 'EOF' )
+/^\*background:/{
+	print "--bg-color",$2
+}
+/^\*foreground:/{
+	print "--font-color",$2
+}
+EOF
