5af43d1197a6e438b03ad943a8907c980ea9a562
xPMo <PMo_@vivaldi.net>
2019-03-05 08:56:48 -0600

    Add args to screenkey

--- BIN/screenkey-wrap.sh
+++ BIN/screenkey-wrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # shellcheck disable=2046
-exec screenkey --font-size small $(xrdb -query | awk -f /dev/fd/3 3<< 'EOF' )
+exec screenkey --font-size small $(xrdb -query | awk -f /dev/fd/3 3<< 'EOF' ) "$@"
 /^\*background:/{
 	print "--bg-color",$2
 }
